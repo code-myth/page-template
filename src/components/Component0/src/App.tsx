2204c6df81953cc44c910577549a151714656826
zhuangjiaxiang <zhuangjiaxiang@meituan.com>
2020-04-21 15:14:23 +0800

    update

---
+++ src/components/Component0/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      body
+    </div>
+  );
+}
+
+export default App;
