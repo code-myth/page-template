19f48aeb60ebce6b6a12f52d399de9aaf2db0445
zhuangjiaxiang <zhuangjiaxiang@meituan.com>
2020-04-21 15:00:38 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+
 import React from 'react';
+import Components0 from './components/Component0/src/App.tsx';
+import Components1 from './components/Component1/src/App.tsx';
 
 function App() {
   return (
     <div className="App">
+      <Components0 />
+<Components1 />
+
     </div>
   );
 }
-
 export default App;
